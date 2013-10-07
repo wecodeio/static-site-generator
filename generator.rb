49e30fb505db5e51bea26a28375e72a64377a71d
Cristian Rasch <cristianrasch@gmail.com>
2013-10-07 16:02:03 -0300

    Use erb instead of an ad-hoc template language

--- generator.rb
+++ generator.rb
@@ -1,17 +1,18 @@
 #! /usr/bin/env ruby
 
 require "yaml"
+require "erb"
+require "ostruct"
 
 phrases = YAML.load(File.read("phrases.yaml"))
-languages = phrases.keys.map { |key| "#${#{key}}" }
-template = File.read("template.html")
+template = ERB.new(File.read("template.erb"))
 
 phrases.each do |language, mappings|
-  t = template.dup
-
-  mappings.each do |key, value|
-    t.gsub!("${#{key}}", value)
+  os = OpenStruct.new(mappings)
+  def os.get_binding
+    binding
   end
+  html = template.result(os.get_binding)
 
-  File.open("template-#{language}.html", "w") { |f| f.write(t) }
+  File.open("template-#{language}.html", "w") { |f| f.write(html) }
 end
